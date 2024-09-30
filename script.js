7335df5924052e3ade141908c3f29f2fdece1612
Runal Manandhar <57617904+Bymax00x@users.noreply.github.com>
2024-09-30 14:01:11 +0545

    feat: exercise three

--- script.js
+++ script.js
@@ -28,13 +28,27 @@ function exerciseOne() {
       }
     }
     
-    console.log("Exercise two answer ",total);
-    alert('exercise two answer '+ total);
+    alert('exercise anwer answer'+ total);
     }
+
+
   document.getElementById("bttnExe1").addEventListener("click", exerciseOne);
   document.getElementById("bttnExe2").addEventListener("click", function() {
       exerciseTwo(3, 5, 1000);
   });
+
+
+  function findTotal() {
+    // Get user input values
+    let a = parseInt(document.getElementById('a').value);
+    let b = parseInt(document.getElementById('b').value);
+    let n = parseInt(document.getElementById('n').value);
+
+    exerciseTwo(a,b,n)
+
+    // Alert the result to the user
+    // alert("The sum of multiples of " + a + " or " + b + " below " + n + " is: " + sum);
+}
   
   
   
